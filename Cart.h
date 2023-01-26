b86544bada39ff608ef311f5fe0fbee02ec98b92
zcaspall <55821382+zcaspall@users.noreply.github.com>
2023-01-25 23:20:16 -0600

    Update Cart.h

--- Cart.h
+++ Cart.h
@@ -16,10 +16,13 @@ public:
     void printCart( );
 private:
     void calculateTotal( ); 
+    // hardcoded the catalog
     std::vector<Product> catalog {{"Red FLower", "RF1", 32.95},
                                   {"Green Flower", "GF1", 24.95},
                                   {"Blue Flower", "BF1", 7.95}};
+    // allows you to add as many or as few shipping prices as needed
     std::vector< std::pair<double, double>> shippingPrices {{50.0, 4.95}, {90.0, 2.95}};
+    // allows you to edit or add different discounts based on product codes
     std::vector<Offer> offers {{"RF1", .5, 2}};
     std::vector<CartItem> cart;
     double total = 0;
